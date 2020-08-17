23950b8bacce0bd3578e43581ffd8534b9993461
amith2233 <69623180+amith2233@users.noreply.github.com>
2020-08-17 18:07:55 +0530

    Add files via upload

---
+++ TestRectangle.java
@@ -0,0 +1,38 @@
+package amith;
+
+import java.util.Scanner;
+
+class Rectangle
+{
+	int width;
+	int height;
+	void display()
+	{
+		System.out.println("width: "+width+" "+"height: "+height);
+	}
+}
+class RectangleArea extends Rectangle
+{
+	void read_input()
+	{
+		Scanner sc=new Scanner(System.in);
+		width=sc.nextInt();
+		height=sc.nextInt();
+		sc.close();
+	}
+	void display()
+	{
+		super.display();
+		System.out.println("area is: "+(width*height));
+	}
+}
+public class TestRectangle {
+
+	public static void main(String[] args) {
+		RectangleArea r1=new RectangleArea();
+		r1.read_input();
+		r1.display();
+		
+	}
+
+}
