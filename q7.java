1541f46584dd6e3d08ab4c32aa82431200a23871
amith2233 <69623180+amith2233@users.noreply.github.com>
2020-08-17 18:19:00 +0530

    Create q7.java

---
+++ q7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math; 
+class Triangle
+{
+	
+	double area(int a,int b,int c)
+	{
+		float s=(a+b+c)/2;
+		float a1=s*(s-a)*(s-b)*(s-c);
+		double area=Math.pow(a1,0.5);
+		return area;
+	}
+}
+public class TriangleArea {
+
+	public static void main(String[] args) {
+		
+Triangle t1=new Triangle();
+Scanner sc=new Scanner(System.in);
+int a,b,c;
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+double area1=t1.area(a,b,c);
+System.out.println("area of triangle is: "+area1);
+sc.close();
+	}
+
+}
